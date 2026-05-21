cef410839506565b37337f55c5d73ea32e86722a
Moha <alisantos743@gmail.com>
2026-05-21 15:05:25 +0300

    submited exrcise 4

---
+++ exercise 4/test.js
@@ -0,0 +1,18 @@
+// function Basics
+console.log("function Basics")
+function add(a, b) {
+    console.log(a + b);
+}
+add(5, 10);
+add(3, 7);
+add(9, 4);
+
+// function expression
+console.log("Function Expressions")
+let add2 = function(a, b) {
+    console.log(a + b);
+}
+add2(5, 12);
+add2(2, 7);
+add2(8, 4);
+
