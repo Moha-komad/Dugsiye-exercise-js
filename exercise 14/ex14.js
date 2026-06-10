95e6ffa2853586bc9b8f542005ecb73219efaecf
Moha <alisantos743@gmail.com>
2026-06-10 10:40:56 +0300

    submitted

---
+++ exercise 14/ex14.js
@@ -0,0 +1,7 @@
+let number 
+do{
+
+    number = parseInt(prompt("Enter a number that is  greater than 10 "))
+}
+while(number <= 10)
+console.log("Thank you for entering a valid number: " + number)
