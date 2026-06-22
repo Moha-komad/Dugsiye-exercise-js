5855a85cebfc2a7acd2cafe6715113824b5fd1df
Moha <alisantos743@gmail.com>
2026-06-22 13:50:48 +0300

    submitted

---
+++ Exercise 22/ex22.js
@@ -0,0 +1,5 @@
+const color = ["red", "green", "blue"];
+const [firstColor, secondColor, thirdColor] = color;
+
+console.log(firstColor);
+console.log(thirdColor);
---
+++ exercise 24/exe24.js
@@ -0,0 +1,9 @@
+function calculateArea(width, height = width) {
+  return width * height;
+}
+
+// Test cases
+console.log(calculateArea(5, 10)); 
+console.log(calculateArea(4));     
+console.log(calculateArea(7, 3));  
+console.log(calculateArea(6));    
